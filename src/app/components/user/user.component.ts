33b637e1818bb9cb225cd6c7a71c2c8a6b1c415c
Brice Basse <bbasse0@gmail.com>
2025-03-30 14:05:37 +0200

    created component ,component Decoration, render Component, Create single file component

---
+++ src/app/components/user/user.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  imports: [],
+  template:"<h1 class='text-danger'>Hi from user Page</h1>",
+  styles: [".text-danger{ color: blue; }"]
+
+  
+})
+export class UserComponent {
+
+}
